c92f17191a724512bc920798fdd39d0e03e39b97
Thomas Sundman--Mathon <sudnam07@gmail.com>
2024-12-01 16:04:03 +0100

    Suppresion des bases de données dans le bon ordre

--- data/deleteDB.sql
+++ data/deleteDB.sql
@@ -1,11 +1,11 @@
+DROP TABLE IF EXISTS Isolation;
+DROP TABLE IF EXISTS Chauffage;
+DROP TABLE IF EXISTS PhotoVoltaique;
 DROP TABLE IF EXISTS Mesures;
+DROP TABLE IF EXISTS Communes;
 DROP TABLE IF EXISTS Departements;
 DROP TABLE IF EXISTS Regions;
 
 --TODO Q4 Ajouter les suppressions des nouvelles tables
 
-DROP TABLE IF EXISTS Communes;
-DROP TABLE IF EXISTS Isolation;
-DROP TABLE IF EXISTS chauffage;
-DROP TABLE IF EXISTS photoVoltaique;
 
